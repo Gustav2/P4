fcbd5e1ff7b44f7f8153fc570d42cf2d779a6b19
maunus <mholms23@student.aau.dk>
2025-04-30 13:16:42 +0200

    Fixed timestamp format for dissector

--- wireshark/wireshark_dissector/spi_dissector.lua
+++ wireshark/wireshark_dissector/spi_dissector.lua
@@ -5,17 +5,12 @@ local proto_spi = Proto.new("SPI", "SPI Protocol")
 function proto_spi.dissector(buffer, pinfo, tree)
 end
 
--- Registering the protocl
--- figure out how to register protocol for the specificed port and protocol here
-
 -- Protocol fields
 local field_miso = ProtoField.bool("spi.miso", "MISO", base.NONE)
 local field_mosi = ProtoField.bool("spi.mosi", "MOSI", base.NONE)
 local field_cs = ProtoField.bool("spi.cs", "CS", base.NONE)
 local field_sclk = ProtoField.uint16("spi.sclk", "SCLK", base.DEC)
 local field_timestamp = ProtoField.uint32("spi.timestamp", "Timestamp", base.DEC)
-
--- We can generate some fields here that we derive from data
 
 -- The thing that is called to display the fields (i think?)
 proto_spi.fields = {
@@ -41,24 +36,21 @@ function proto_spi.dissector(buffer, pinfo, tree)
   local fifth_byte = buffer(4, 1):uint()
   local sixth_byte = buffer(5, 1):uint()
 
-  -- The first 3 bits (miso, mosi and cs) in the first byte
+  -- Timestamp values in last four bytes
+  local timestamp = buffer(2, 4):uint() -- the actual timestamp in clock cycles
+  local timestamp_bytes = string.format("%d %d %d %d", third_byte, 
+                                        fourth_byte, fifth_byte, sixth_byte) -- the four timestamp bytes as string
+
+  -- Extract bits 3-15 (sclk_freq)
+  local sclk_freq = bit.rshift(bit.band(first_byte, 0xF8), 3) -- Extract bits 3-7 and shift them to the least significant position
+  sclk_freq = bit.bor(sclk_freq, bit.lshift(second_byte, 5)) -- Combine with bits 8-15
+
+  -- Adding all the tree items (displays in the order they are added but columns must be manually configured)
+  payload_tree:add(field_cs, buffer(0, 1), bit.band(first_byte, 0x20) ~= 0) -- bit 5
   payload_tree:add(field_miso, buffer(0, 1), bit.band(first_byte, 0x80) ~= 0) -- bit 7
   payload_tree:add(field_mosi, buffer(0, 1), bit.band(first_byte, 0x40) ~= 0) -- bit 6
-  payload_tree:add(field_cs, buffer(0, 1), bit.band(first_byte, 0x20) ~= 0) -- bit 5
-  
-  -- Extract bits 3-15 (sclk_freq)
-  local sclk_freq = bit.band(first_byte, 0xF8) -- Get bits 3-7 of the first byte
-  sclk_freq = bit.bor(sclk_freq, bit.lshift(second_byte, 3)) -- Combine with bits 8-15
   payload_tree:add(field_sclk, buffer(0, 2), sclk_freq)
-
-  -- Extract bits 16-47 (timestamp)
-  local timestamp = bit.bor(
-    third_byte,
-    bit.lshift(fourth_byte, 8),
-    bit.lshift(fifth_byte, 16),
-    bit.lshift(sixth_byte, 24)
-  )
-  payload_tree:add(field_timestamp, buffer(2, 4), timestamp)
+  payload_tree:add(field_timestamp, buffer(2, 4), timestamp):append_text(" (Bytes: ".. timestamp_bytes ..")")
 
 end
 
